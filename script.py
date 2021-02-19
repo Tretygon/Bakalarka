8d4f492d36057325a56e4352afb344f5d2212b9d
Tomas Zasadil <TomasZasadil@gmail.com>
2021-02-19 22:31:38 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import copy
 import functools
+from tkinter.constants import E
 import numpy as np
+import sklearn
 import typing
 from typing import List,Tuple
 import random
@@ -14,27 +16,47 @@ from dataclasses import dataclass
 import pydub
 from pydub.playback import play
 
+
+MAX_ITER = 10
+
+
 @dataclass
 class recording_info:
     rec_name:str
     seqs: List[Tuple[float,float]]
 
-all_recording_infos : List[recording_info] = []
-all_recording_files: List[str] = []
 
-
-def load_excels(): 
+def load_files_dialog(): 
     root = tkinter.Tk()
     selected = tkinter.filedialog.askopenfilenames(parent=root, title='Choose files')
     files = root.tk.splitlist(selected)
     for file in files: process_file(file)
 
-def process_file(file_name: str):
-    suffix = "DOPO" if "DOPO" in file_name else "RANO" if "RANO" in file_name else None
-    if suffix is None : raise Exception("invalid file name:  "+file_name)
 
-    wb = xl.load_workbook(filename = file_name)
+def load_data_recursively(root:str) -> 'tuple[List[recording_info],List[str]]' : 
+    from pathlib import Path
+
+
+    all_recording_infos : List[recording_info] = []
+    for p in Path(root).rglob('*.xlsx'):
+        s = p.absolute().__str__()
+        i = process_file(s)
+        all_recording_infos += i
+
+    all_audio_files : List[str] = []
+    for p in Path(root).rglob('*.wav'):
+        s = p.absolute().__str__()
+        all_audio_files.append(s) 
+ 
+    return all_recording_infos, all_audio_files
+
+def process_file(file_path: str)->List[recording_info]:
+    suffix = "DOPO" if "DOPO" in file_path else "RANO" if "RANO" in file_path else None
+    if suffix is None : raise Exception("invalid file name:  "+file_path)
+
+    wb = xl.load_workbook(filename = file_path)
     info : recording_info = recording_info("", [])
+    all_recording_infos : List[recording_info] = []
 
     def store():
         if info.rec_name != "" and len(info.seqs) != 0:
@@ -47,14 +69,35 @@ def process_file(file_name: str):
             info = recording_info(r[2].value+suffix+".wav" ,[])
         info.seqs.append((r[14].value * 1000, r[15].value * 1000))
     store()
+    return all_recording_infos
 
 
 def load_recording(info:  recording_info):
     song = pydub.AudioSegment.from_wav(info.file_name)
     for [start, end] in  info.seqs:
-        #TODO  feature engineering
-        seg = song[start:end].set_sample_width(8).set_channels(1).set_frame_rate(10_000)
+        #TODO  feature engineering stuff
+        seg = song[start:end].set_frame_rate(10_000).set_sample_width(8)
         play(seg)
+
+
+# train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
+#         dataset.data, dataset.target, test_size=args.test_size, random_state=args.seed)
+
+def train(data,t):
+    pipe = sklearn.pipeline.Pipeline(
+        [("scaling", sklearn.preprocessing.StandardScaler())] +
+        [("classifier", sklearn.linear_model.LogisticRegression(solver="saga", max_iter=MAX_ITER))]
+    )
+    pipe.fit(data, t)
+    return pipe
+ 
+
+
+def classify(model, data):
+    return model.predict(data)
+
+
+
 
 
 
